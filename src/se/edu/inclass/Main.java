0bb7ca750f028b2422ef3e355627b832f6571503
Zong Xian <zongxian96@gmail.com>
2020-09-18 17:06:41 +0800

    Added printDataUsingStreams

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -22,6 +22,12 @@ public class Main {
 
         System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
 
+        printDataUsingStreams(tasksData);
+
+        printDeadlinesUsingStreams(tasksData);
+
+        System.out.println("Total number of deadlines using streams:" + countDeadlinesUsingStreams(tasksData));
+        
     }
 
     private static int countDeadlines(ArrayList<Task> tasksData) {
