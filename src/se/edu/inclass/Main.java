a4d2e900e217eb26f3e8cacc58237c6692dc20f3
Zong Xian <zongxian96@gmail.com>
2020-09-18 17:07:18 +0800

    Added printDeadlinesUsingStreams

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -53,4 +53,12 @@ public class Main {
             }
         }
     }
+
+    public static void printDeadlinesUsingStreams(ArrayList<Task> tasksData) {
+        System.out.println("Printing deadlines using streams");
+        tasksData.stream()
+                .filter((t) -> t instanceof Deadline)
+                .forEach(System.out::println);
+
+    }
 }
